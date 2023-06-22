e6d7b0fa7e3593288cb29c7d98ab74842bbccd72
Nathan Schroeder <nathan.schroeder@REM-NathanS.local>
2023-06-22 12:01:38 -0400

    message

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import './App.css';
+import { getChartContext, ChartModel, ChartConfig, ColumnType, CustomChartContext, Query } from '@thoughtspot/ts-chart-sdk';
+import React from 'react';
+import _ from 'lodash';
+
+
+(async () => {
+    const ctx = await getChartContext({
+        getDefaultChartConfig: (chartModel: ChartModel): ChartConfig[] => {
+            const cols = chartModel.columns;
+
+            const measureColumns = _.filter(cols, (col) => col.type === ColumnType.MEASURE);
+
+            const attributeColumns = _.filter(cols, (col) => col.type === ColumnType.ATTRIBUTE);
+
+            const axisConfig: ChartConfig = {
+                key: 'default',
+                dimensions: [
+                    {
+                        key: 'x',
+                        columns: [attributeColumns[0]],
+                    },
+                    {
+                        key: 'y',
+                        columns: measureColumns.slice(0, 2),
+                    },
+                ],
+            };
+            return [axisConfig];
+        },
+        getQueriesFromChartConfig: (
+            chartConfig: ChartConfig[],
+        ): Array<Query> => chartConfig.map((config: ChartConfig): Query => _.reduce(
+            config.dimensions,
+            (acc: Query, dimension) => ({
+                queryColumns: [...acc.queryColumns, ...dimension.columns],
+            }),
+            {
+                queryColumns: [],
+            } as Query,
+        )),
+        renderChart: (ctx) => renderChart(ctx),
+    });
+
+    renderChart(ctx);
+})();
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+function renderChart(ctx: CustomChartContext): Promise<void> {
+    throw new Error('Function not implemented.');
+}
+
